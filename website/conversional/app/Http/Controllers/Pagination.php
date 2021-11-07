05e49487095a7ed0391da6b363004319b8cd8502
Azibom <mrsh13610@gmail.com>
2021-11-07 15:39:01 +0330

    add pagination class

---
+++ website/conversional/app/Http/Controllers/Pagination.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class Pagination
+{
+    const INVOICE_DEFAULT_USER_LIMIT = 100;
+    const INVOICE_DEFAULT_USER_OFFSET = 0;
+    const INVOICE_DEFAULT_LIMIT = 100;
+    const INVOICE_DEFAULT_OFFSET = 0;
+}
