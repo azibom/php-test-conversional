359d0b2a003412ef9f08c981645d2879f1976b6d
Azibom <mrsh13610@gmail.com>
2021-11-07 15:42:07 +0330

    update service provider

--- website/conversional/app/Providers/AppServiceProvider.php
+++ website/conversional/app/Providers/AppServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Interfaces\InvoicePriceServiceInterface;
+use App\Interfaces\InvoicePriceStrategyInterface;
+use App\Interfaces\InvoiceRepositoryInterface;
+use App\Interfaces\InvoiceServiceInterface;
+use App\Repositories\InvoiceRepository;
+use App\Services\InvoicePriceService;
+use App\Services\InvoiceService;
+use App\Services\InvoiceServiceWithCache;
+use App\Strategies\InvoicePriceStrategy;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // $this->app->bind(InvoiceServiceInterface::class, InvoiceServiceWithCache::class);
+        $this->app->bind(InvoiceServiceInterface::class, InvoiceService::class);
+        $this->app->bind(InvoiceRepositoryInterface::class, InvoiceRepository::class);
+        $this->app->bind(InvoicePriceServiceInterface::class, InvoicePriceService::class);
+        $this->app->bind(InvoicePriceStrategyInterface::class, InvoicePriceStrategy::class);
     }
 
     /**
